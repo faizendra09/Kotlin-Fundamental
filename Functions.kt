529997bda9b1da9929b7adef6a42b55a1e55a9c3
faizendra09 <53174135+faizendra09@users.noreply.github.com>
2020-09-19 11:10:30 +0700

    Add files via upload

---
+++ Functions.kt
@@ -0,0 +1,5 @@
+fun main(){
+    val user = setUser("Faiz", 15)
+    println(user)
+}
+fun setUser (name:String, age: Int) = "My name is $name, I like watching movie, and I'm $age years old"
---
+++ HelloKotlin.kt
@@ -0,0 +1,9 @@
+fun main() {
+    val name = "Faiz Endra Raditya"
+    val hobby = "Nonton Film"
+
+    print("Nama saya ")
+    println(name)
+    print("Hobi saya ")
+    println(hobby)
+}
